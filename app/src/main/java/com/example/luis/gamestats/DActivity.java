48a98a42e3e2f00979554f12f984a46dbcaad6ce
Francisco Robleto <frobleto323@gmail.com>
2016-04-14 15:44:19 -0500

    added version 1 of app

---
+++ app/src/main/java/com/example/luis/gamestats/DActivity.java
@@ -0,0 +1,14 @@
+package com.example.luis.gamestats;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d);
+    }
+
+}
---
+++ app/src/main/java/com/example/luis/gamestats/Players.java
@@ -0,0 +1,14 @@
+package com.example.luis.gamestats;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Players extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players);
+    }
+
+}
